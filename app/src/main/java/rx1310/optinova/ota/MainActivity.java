20c2fc2b55bc7da8a97086f299f8e3573b99c175
rx1310 <rx1310@inbox.ru>
2020-11-22 12:17:58 +0300

    test

--- app/src/main/java/rx1310/optinova/ota/MainActivity.java
+++ app/src/main/java/rx1310/optinova/ota/MainActivity.java
@@ -1,14 +1,46 @@
 package rx1310.optinova.ota;
 
-import android.app.*;
-import android.os.*;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import com.loveplusplus.update.AppUtils;
+import com.loveplusplus.update.UpdateChecker;
+import rx1310.optinova.ota.MainActivity;
 
-public class MainActivity extends Activity 
-{
+public class MainActivity extends Activity
+ {
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Button btn1 = findViewById(R.id.button1);
+        Button btn2 = findViewById(R.id.button2);
+
+        btn1.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					UpdateChecker.checkForDialog(MainActivity.this);
+				}
+			});
+			
+        btn2.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					UpdateChecker.checkForNotification(MainActivity.this);
+				}
+			});
+
+
+        TextView textView = findViewById(R.id.textView1);
+
+        textView.setText("v: versionName = " + AppUtils.getVersionName(this) + " versionCode = " + AppUtils.getVersionCode(this));
     }
+
 }
