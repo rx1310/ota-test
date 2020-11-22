9362b2511fb6ae49507b60e4050b107cbae5158c
rx1310 <rx1310@inbox.ru>
2020-11-22 11:57:11 +0300

    Test

---
+++ app/build/gen/rx1310/optinova/ota/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package rx1310.optinova.ota;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ app/src/main/java/rx1310/optinova/ota/MainActivity.java
@@ -0,0 +1,14 @@
+package rx1310.optinova.ota;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
